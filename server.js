beafb7cb10ef53101896318aee8908c8ed02f9e8
Jason C <jayingmantis.555@gmail.com>
2021-06-11 16:29:16 -0500

    tip calculator express app created

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/greeting/', (req, res) => {
 app.get('/greeting/:name', (req, res) => {
   res.send(`Hello ${req.params.name}`);
 });
+/***************************************/
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+  const total = parseInt(req.params.total);
+  const tipPercentage = parseInt(req.params.tipPercentage);
+  const final = total * tipPercentage / 100;
+  res.send(`${final}`);
+});
 
 
 
